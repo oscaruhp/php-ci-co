be7576def8b24b47edbcf142f593a44a46c4d371
Develoteca <dimit28@gmail.com>
2021-09-14 11:55:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 CRUD php pg 
 <?php 
 
-$contraseña = "6b4b3689cc69d05ca17b16c509dac33b4a1bd961c3efe64fd554ac3ddfb356dc";
-$usuario = "mfeavnsxdipavd";
-$nombreBaseDeDatos = "ddb5abrgnl1ug";
+$contraseña = "";
+$usuario = "";
+$nombreBaseDeDatos = "";
 # Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
 $rutaServidor = "ec2-54-81-126-150.compute-1.amazonaws.com";
 $puerto = "5432";
