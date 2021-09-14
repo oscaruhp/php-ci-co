bdfc14c6faf2e80f72c9544caaafd1de70ad1a57
Develoteca <dimit28@gmail.com>
2021-09-14 10:59:50 -0500

    conexión

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 
+<?php 
+
+$contraseña = "hunter2";
+$usuario = "parzibyte";
+$nombreBaseDeDatos = "mascotas";
+# Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
+$rutaServidor = "127.0.0.1";
+$puerto = "5432";
+try {
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
+
+?>
