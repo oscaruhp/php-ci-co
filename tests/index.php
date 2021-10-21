ef9690b0b45da289be033594d29b8ae55e78fcc7
Develoteca <dimit28@gmail.com>
2021-10-21 13:16:21 -0500

    Form suma
    Formulario de Suma

---
+++ tests/index.php
@@ -0,0 +1,39 @@
+<?php 
+use App\Classes\Calc;
+if($_POST){
+$calc = new Calc();
+$result = $calc->sum(1, 2);
+echo "El resultado es:".$result;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        
+    <div class="mb-3">
+      <label for="" class="form-label">Valor A:</label>
+      <input type="text"
+        class="form-control" name="valora" id="valora" aria-describedby="helpId" placeholder="Valor A">
+      
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label">Valor B:</label>
+      <input type="text"
+        class="form-control" name="valora" id="valorb" aria-describedby="helpId" placeholder="Valor B">
+      
+    </div>
+    
+    <input name="" id="" class="btn btn-danger" type="button" value="Sumar">
+
+    </form>    
+
+
+</body>
+</html>
